41ff260eaabe8ce590441cb7166aafabdbbd86e3
satoyuki <yuki01377@gmail.com>
2020-08-11 23:50:05 +0900

    vue 変更

--- app/javascript/src/components/home/index.vue
+++ app/javascript/src/components/home/index.vue
@@ -1,5 +1,71 @@
 <template>
   <div>
-    hello index.vue
+    <modal 
+      v-bind:isShow="isShow" 
+      @toggleShow="toggleShow"
+    />
+    <h1>試合表</h1>
+    <span>2020/8/6(水)</span>
+    <button type="button" class="print" onclick="window.print();">印刷</button>
+    <div class="match-table">
+      <div class="match-table__list">
+        <div class="match-table__list--cell">佐藤</div>
+        <div class="match-table__list--cell2" v-on:click="toggleShow">
+          <!-- <%= image_tag 'men.svg', class: 'match-table__icon' %>
+          <%= image_tag 'men.svg', class: 'match-table__icon' %> -->
+          <img src="" alt="">
+        </div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell">田中</div>
+      </div>
+      <div class="match-table__list">
+        <div class="match-table__list--cell">鈴木</div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell">武藤</div>
+      </div>
+      <div class="match-table__list">
+        <div class="match-table__list--cell">鈴木</div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell">武藤</div>
+      </div>
+      <div class="match-table__list">
+        <div class="match-table__list--cell">鈴木</div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell">武藤</div>
+      </div>
+      <div class="match-table__list">
+        <div class="match-table__list--cell">鈴木</div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell2"></div>
+        <div class="match-table__list--cell">武藤</div>
+      </div>
+      <div class="match-table__list">
+        <div class="match-table__list--cell">○○高校</div>
+        <div class="match-table__list--cell">△△高校</div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import modal from './modal.vue';
+export default {
+  components: {
+    modal,
+  },
+  name: "index",
+  data: () => {
+    return {
+      member: 5,
+      isShow: false,
+    }
+  },
+  methods: {
+    toggleShow() {
+      this.isShow = !this.isShow;
+    }
+  }
+}
+</script>
---
+++ app/javascript/src/components/home/modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mask" v-if="isShow" v-on:click.self="onToggleShow">
+    <div class="m-card">
+      <button type="button" class="m-card__close-btn" v-on:click="onToggleShow">閉じる</button>
+      <h2>試合表を変更</h2>
+      <div class="m-card__list">
+        <!-- <img src="/app/javascript/src/assets/images/men.svg" alt="">
+        <img src="/app/javascript/src/assets/images/sankaku.svg" alt="">
+        <img src="/app/javascript/src/assets/images/ippon.svg" alt=""> -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "modal",
+  props: [
+    'isShow'
+  ],
+  methods: {
+    onToggleShow() {
+      this.$emit('toggleShow');
+    }
+  }
+}
+</script>
