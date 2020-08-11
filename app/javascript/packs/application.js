4cb4b62260d9c7c9dea6bbe3fd4cb50ecf6b839e
satoyuki <yuki01377@gmail.com>
2020-08-11 09:41:45 +0900

    scss add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,24 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
 // console.log('Hello World from Webpacker')
 import Vue from 'vue/dist/vue.esm';
 import router from '../src/router.js';
+import '../src/scss/style.scss';
 
 const app = new Vue({
   router,
